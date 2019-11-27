b8465340d57d964332c8764d26125fd47cb81442
Normenxx <duer.normen@gmx.at>
2019-11-27 14:08:37 +0100

    Initial commit

---
+++ src/main/java/dev/norman/testnorman/TestnormanApplication.java
@@ -0,0 +1,14 @@
+package dev.norman.testnorman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class TestnormanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestnormanApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/norman/testnorman/TestnormanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.norman.testnorman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestnormanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
