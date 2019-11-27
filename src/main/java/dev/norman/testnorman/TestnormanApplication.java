6f5cea1cc13c1204390f6b7b8c521f829f7b49f3
Normenxx <duer.normen@gmx.at>
2019-11-27 14:11:03 +0100

    Gewünschte Funktionalität erstellt

--- src/main/java/dev/norman/testnorman/TestnormanApplication.java
+++ src/main/java/dev/norman/testnorman/TestnormanApplication.java
@@ -9,6 +9,30 @@ import java.util.Scanner;
 public class TestnormanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TestnormanApplication.class, args);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("The World's Finest Calculator!");
+
+        double zahl1;
+        do{
+            System.out.println("Geben Sie bitte Zahl 1 ein (0 nicht erlaubt):");
+            zahl1 = scanner.nextDouble();
+        } while (zahl1 == 0);
+
+
+        double zahl2 = 0;
+
+        do{
+            System.out.println("Geben Sie bitte Zahl 2 ein (0 nicht erlaubt):");
+            zahl2 = scanner.nextDouble();
+        } while (zahl2 == 0);
+
+
+        double ergebnis =  (zahl1 + zahl2) / (zahl1 * zahl2);
+
+        System.out.println("Ergebnis (z1+z2)/(z1*z2):" + ergebnis);
+
+        //SpringApplication.run(TestnormanApplication.class, args);
     }
 }
